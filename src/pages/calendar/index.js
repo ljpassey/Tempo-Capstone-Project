d16fafb221f2ea2b96b29ac5dabc8c0bb4ebef1f
Luke Passey <ljpassey@gmail.com>
2023-03-21 22:47:22 -0600

    Updated commit

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -3,7 +3,15 @@ import CalendarComponent from "./calendar";
 
 const Calendar = () => {
   return (
-    <div style={{ position: 'relative', left: '500px', width: "auto", height: "auto" }}>
+    <div
+      style={{
+        display: "flex",
+        position: "relative",
+        left: "500px",
+        width: "auto",
+        height: "auto",
+      }}
+    >
       <CalendarComponent />
     </div>
   );
